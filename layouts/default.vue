6493c7e88783f28bd4f6ee7a7709a149396c329d
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2023-08-20 02:14:24 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="flex justify-center mt-10">
+      <NuxtLink class="px-4 py-2 border rounded" to="/"> Home </NuxtLink>
+    </nav>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ pages/movies/[id].vue
@@ -0,0 +1,4 @@
+<template>
+  <h1> Movie Details </h1>
+</template>
+
