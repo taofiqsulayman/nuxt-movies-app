04928db6317cc1f78336dd44079a15ccd39e3a3e
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2023-09-09 21:08:09 +0100

    refactor casts card

--- components/Casts.vue
+++ components/Casts.vue
@@ -3,7 +3,7 @@
     <div v-for="cast in castList" :key="cast.id" class="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 p-4">
       <div class="text-center">
         <img
-          :src="`${config.public.imageUrl}/${cast.profile_path}`"
+          :src="getCastImageUrl(cast.profile_path)"
           :alt="cast.name"
           class="rounded-full mx-auto h-32 w-32 object-cover"
         />
@@ -15,12 +15,15 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
 
 const props = defineProps({
   castList: Array,
   config: Object,
 });
+
+const getCastImageUrl = (imageUrl) => {
+  return imageUrl ? `${props.config.public.imageUrl}/${imageUrl}` : 'https://via.placeholder.com/300x500';
+};
 
 </script>
 
