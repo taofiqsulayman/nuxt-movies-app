332e90f90e6bc47e5582504c057de2db35ca74ad
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2024-05-27 01:28:32 +0100

    chore: style upgrade for home button

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex self-center justify-center mt-10">
-      <NuxtLink class="text-2xl font-bold text-center bg-teal-400 text-slate-100 rounded-lg mx-4 my-2 animate-pulse" to="/">
+      <NuxtLink class="custom-link" to="/">
         Movie App
       </NuxtLink>
     </div>
@@ -18,3 +18,34 @@
 // check if its the homepage
 const isHome = useRoute().path === '/';
 </script>
+
+
+<style scoped>
+.custom-link {
+  display: inline-block;
+  padding: 0.5rem 1.5rem;
+  font-size: 1.5rem; 
+  font-weight: bold;
+  text-align: center;
+  background-color: #38b2ac; 
+  color: #f1f5f9; 
+  border-radius: 0.5rem; 
+  margin: 0.5rem 1rem; 
+  animation: pulse 2s infinite;
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.custom-link:hover {
+  transform: translateY(-3px);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+@keyframes pulse {
+  0%, 100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  }
+}
+</style>
